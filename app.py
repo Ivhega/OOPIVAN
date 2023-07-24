ab99dd1f5a6df9c8e985c94d8e4f45321abde685
Ivan <ivhega@gmail.com>
2023-07-24 13:06:04 -0600

    Class Person, Methods of color and age+10 Added

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 # add your name to the print statement below
 print("Hello, my name is " + "Ivan")
+
+class Person:
+    def __init__(self,personID,firstName,lastName,favoriteColour,age,isWorking):
+        self.person_ID = personID
+        self.first_Name = firstName
+        self.last_Name = lastName
+        self.favorite_Colour = favoriteColour
+        self.age = age
+        self.isWorking = isWorking
+    def change_favorite_colour(self):
+        self.favorite_Colour = "white"
+    def get_age_in_ten_years(self):
+        print(f"Customer name: {self.age+10}")
+
+a = Person(915433,"Ivan","Hernandez","Blue",36,"Not")
