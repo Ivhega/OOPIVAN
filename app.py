3749cc109a305a538c60ee20d851a1b90e00cdaf
Munir <batmanrobin216@outliers.pw>
2023-07-28 10:07:12 -0600

    add parameter new_color to function change_favorite_colour

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 # add your name to the print statement below
 print("Hello, my name is " + "Ivan")
 
+
 class Person:
-    def __init__(self,personID,firstName,lastName,favoriteColour,age,isWorking):
+    def __init__(self, personID, firstName, lastName, favoriteColour, age, isWorking):
         self.person_ID = personID
         self.first_Name = firstName
         self.last_Name = lastName
         self.favorite_Colour = favoriteColour
         self.age = age
         self.isWorking = isWorking
-    def change_favorite_colour(self):
-        self.favorite_Colour = "white"
+
+    def change_favorite_colour(self, new_color):
+        self.favorite_Colour = new_color
+
     def get_age_in_ten_years(self):
         print(f"Age in ten year will be: {self.age+10}")
 
-a = Person(915433,"Ivan","Hernandez","Blue",36,"Not")
+
+a = Person(915433, "Ivan", "Hernandez", "Blue", 36, "Not")
 
 a.get_age_in_ten_years()
